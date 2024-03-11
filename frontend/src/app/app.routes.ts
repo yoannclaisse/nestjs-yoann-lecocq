dadbe2b4498d37cdcff8ba1aa26132053da04ac6
yoannclaisse <claisse.yoann@gmail.com>
2024-03-11 16:36:06 +0100

    add account route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SigninComponent } from './signin/signin.component';
+import { AccountComponent } from './account/account.component';
 
 export const routes: Routes = [
     // { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'signin', component: SigninComponent },
+    { path: 'account', component: AccountComponent },
 ];
