f8627164dfa2e175bf31946bbc6cb722623266e6
yoannclaisse <claisse.yoann@gmail.com>
2024-03-11 16:53:26 +0100

    add rout logout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SigninComponent } from './signin/signin.component';
 import { AccountComponent } from './account/account.component';
+import { LogoutComponent } from './logout/logout.component';
 
 export const routes: Routes = [
     // { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
+    { path: 'logout', component: LogoutComponent },
     { path: 'signin', component: SigninComponent },
     { path: 'account', component: AccountComponent },
 ];
