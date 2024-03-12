e6e1bc4e8d05f07dca06f52faaf64f319290d4c5
yoannclaisse <claisse.yoann@gmail.com>
2024-03-12 15:31:57 +0100

    add crud for account component

--- frontend/src/app/account/account.component.ts
+++ frontend/src/app/account/account.component.ts
@@ -1,15 +1,83 @@
 import { Component } from '@angular/core';
 import { GraphqlService } from '../graphql.service';
+import { FormsModule } from '@angular/forms';
+import { Todo, User } from '../models/graphql.model';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './account.component.html',
   styleUrl: './account.component.css'
 })
 export class AccountComponent {
+
+  resultStatus: String = ""
+
+  newTitle: String = ""
+
+  newDescription: String = ""
+
   constructor(public graphqlService: GraphqlService) {
     console.log (this.graphqlService.user)
+  }
+
+  addTodo(/*title: String, description: String*/) {
+    console.log('test addtodo')
+    if (!this.newTitle) {
+      this.resultStatus = "no title define"
+      console.log(this.resultStatus)
+      return
+    }
+    if (!this.newDescription) {
+      this.resultStatus = "no description define"
+      console.log(this.resultStatus)
+      return
+    }
+    if (!this.graphqlService.user) {
+      this.resultStatus = "no user defined"
+      console.log(this.resultStatus)
+      return
+    }
+    // 
+    this.graphqlService.createTodo(this.newTitle, this.newDescription, this.graphqlService.user.id)
+      .subscribe(
+        (todo: Todo) => {
+          // Cette méthode "push" ne fonctionne pas pck les props de this.user sont en lecture seule
+          // this.user.todos.push(todo)
+          // A la place, on remplace la props this.user.todos par elle même avec le todo fraîchement crée
+          // this.user.todos = [...this.user.todos, todo]
+          //  Ajoute un controle si this.user.todos est vide
+          // On utilise le "!!" pour verif this.user.todos n'est pas vide/null/undifined
+          
+          // nettoie les champs
+          this.newTitle = '';
+          this.newDescription = '';
+        }
+      )
+  }
+
+  changeTodo(id: Number, updatedComplete: Boolean, updatedTitle: String, updatedDescription: String) {
+    let updatedTodo = this.graphqlService.user?.todos.find(todo => todo.id == id)
+    console.log("variable :", updatedTodo)
+    if (!!updatedTodo) {
+      // updatedTodo.title = updatedTitle
+      updatedTodo = { ...updatedTodo, title: updatedTitle, description: updatedDescription, completed: updatedComplete };
+      console.log(updatedTodo)
+      this.graphqlService
+        .updateTodo(updatedTodo).subscribe((todo: Todo) => {
+          
+        }, (error: any) => {
+          console.log("ERROR :", error)
+          this.resultStatus = error
+        })
+    }
+  }
+
+  removeTodo(id: Number) {
+    this.graphqlService
+      .deleteTodo(id).subscribe((result: Todo) => {
+
+      })
   }
 }
--- frontend/src/app/graphql.service.ts
+++ frontend/src/app/graphql.service.ts
@@ -3,7 +3,7 @@ import { Apollo, MutationResult } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { ApolloQueryResult, InMemoryCache, gql } from '@apollo/client/core';
 import { environment } from '../environments/environment.development';
-import { User, UserQueryResponse } from './models/graphql.model';
+import { Todo, TodoQueryResponse, User, UserQueryResponse } from './models/graphql.model';
 import { Observable } from 'rxjs';
 import { sha256 } from 'js-sha256';
 
@@ -12,7 +12,7 @@ import { sha256 } from 'js-sha256';
 // { "user": {"email": {"equals": "test@test.com"}, "password": {"equals": "azerty789"}} }
 const getUserByEmailAndPassword = gql`query getUserByEmailAndPassword($user: UserWhereInput!){
   findFirstUserOrThrow(where: $user) {
-    id, username, email}
+    id, username, email, todos{description id title completed}}
   }`
 
 // { "createOneUser": {"username": "yoyo3", "email":"yoyo@yoyo.com", "password": "azerty789", "todos": []} }
@@ -20,6 +20,30 @@ const createOneUser = gql`mutation createOneUser($createOneUser: UserCreateInput
   createOneUser(data: $createOneUser){
     id username email}
   }`
+
+const CREATE_TODO = gql`mutation CreateTodo($input: TodoCreateInput!) {
+  createOneTodo(data: $input) {
+    id
+    title
+    description
+    userId
+  }
+}`
+
+const GET_USER = gql`query getUserByNameWithTodos($input: UserWhereUniqueInput!){user(where: $input){id username todos{description id title completed}}}`
+
+const UPDATE_TODO = gql`mutation($input: TodoUpdateInput! $where: TodoWhereUniqueInput!)
+{
+  updateOneTodo(data: $input where: $where)
+  {
+    title
+    description
+    completed
+    id
+  }
+}`
+
+const DELETE_TODO = gql`mutation($input: TodoWhereUniqueInput!) {deleteOneTodo(where: $input){id}}`
 
 @Injectable({
   providedIn: 'root'
@@ -42,6 +66,13 @@ export class GraphqlService {
       // Fetches from http://my-prod-url in production, http://my-dev-url in development.
       link: httpLink.create({ uri: environment.apiUrl }),
     })
+    const username = localStorage.getItem('username') || ""
+    console.log(username)
+    this.getUserTodos(username).subscribe((result: User) => {
+      this.user = result
+    }, (error: any) => {
+      console.log("ERROR :", error)
+    })
   }
 
   // ICI je  m'occupe de faire la query pour le login, elle retourne un observable et on analyse le sub et l'error
@@ -62,6 +93,7 @@ export class GraphqlService {
             const user = result.data.findFirstUserOrThrow
             if (!!user) {
               this.user = user
+              localStorage.setItem('username', this.user.username)
               // on déclenche l'evt "next"
               subscriber.next(user)
             }
@@ -112,10 +144,143 @@ export class GraphqlService {
   logout(): boolean {
     if (!!this.user) {
       this.user = null
+      localStorage.removeItem('username')
       return true
     } else {
       return false
     }
 
+  }
+
+  updateTodo(todo: Todo): Observable<Todo> {
+    return new Observable<Todo>((subscriberEvent) => {
+      this.apollo.mutate<TodoQueryResponse>({
+        mutation: UPDATE_TODO,
+        variables: {
+          "input": { "title": { "set": todo.title }, "description": { "set": todo.description }, "completed": { "set": todo.completed } },
+          "where": { "id": todo.id }
+        }
+      }).subscribe((result: MutationResult<TodoQueryResponse>) => {
+        console.log("Result :", result)
+        let todo = result?.data?.updateOneTodo
+        if (!!todo) {
+          console.log("Result :", todo)
+          // TODO: update this.user
+          // on supprime l'ancien todo non mis à jour
+          const todoId = todo.id
+          let todos = this.user?.todos.filter(item => item.id != todoId)
+          // On verif que la liste restante n'est pas vide
+          if (!!todos) {
+            // On ajoute le todo mis à jour
+            todos = [...todos, todo]
+          } else {
+            // On crée un tableau avec celui existant
+            todos = [todo]
+          }
+          // On affect le nouveau tableau dans this.user
+          if (!!this.user) {
+            this.user.todos = [...todos];
+          }
+          subscriberEvent.next(todo)
+        } else {
+          subscriberEvent.error("unable to update Todo")
+        }
+      }, (error: any) => {
+        subscriberEvent.error(error)
+      })
+    })
+  }
+
+  createTodo(title: String, description: String, userId: Number): Observable<Todo> {
+    console.log('1')
+    return new Observable<Todo>((subscriber) => {
+      console.log('1')
+      this.apollo.mutate<TodoQueryResponse>({
+        mutation: CREATE_TODO,
+        variables: { "input": { "title": title, "description": description, "user": { "connect": { "id": userId } } } }
+      }).subscribe((result: MutationResult<TodoQueryResponse>) => {
+        console.log("Result :", result)
+        let todo = result?.data?.createOneTodo
+        if (!!todo) {
+          if (!!this.user) {
+            if (!!this.user.todos) {
+              this.user = { ...this.user, todos: [...this.user.todos, todo] };
+            } else {
+              this.user = { ...this.user, todos: [todo] };
+            }
+          }
+          subscriber.next(todo)
+        } else {
+          subscriber.error("unable to create Todo")
+        }
+      }, (error: any) => {
+        subscriber.error(error)
+      })
+    })
+  }
+
+  deleteTodo(todoId: Number): Observable<Todo> {
+    return new Observable<Todo>((subscriberEvent) => {
+      this.apollo.mutate<TodoQueryResponse>({
+        mutation: DELETE_TODO,
+        variables: { "input": { "id": todoId } }
+      }).subscribe((result: MutationResult<TodoQueryResponse>) => {
+        console.log("Result :", result)
+        let todo = result?.data?.deleteOneTodo
+        if (!!todo) {
+          console.log("Result :", result)
+          // le filter est un foreach avec un if dedand et retourne un autre tableau filtrer par le if
+          const todos = this.user?.todos.filter(todo => todo.id != todoId)
+          if (!!this.user && !!todos) {
+            this.user.todos = [...todos];
+          }
+          subscriberEvent.next(todo)
+        } else {
+          subscriberEvent.error("unable to delete Todo")
+        }
+      }, (error: any) => {
+        subscriberEvent.error(error)
+      })
+    })
+  }
+
+  // Méthode de classe qui est une focntion qui récupère le user et les todos qui lui sont affectés
+  getUserTodos(username: String): Observable<User> {
+    // On stock l'observable valueChanges dans graphqlRequest
+    const graphqlRequest =
+      this.apollo.watchQuery<UserQueryResponse>({
+        query: GET_USER,
+        variables: { "input": { "username": username } },
+        fetchPolicy: "no-cache"
+      }).valueChanges
+
+    // On crée et retourne un Observable de User et on definit le comportement de l'observable
+    // Le subscriber définit le comportement lorsqu'on va faire un appel .subscribe
+    return new Observable<User>(
+      (subscriber) => {
+        // on subscribe a graphqlRequest pour recup' le resultat de la requêt graphQL
+        graphqlRequest.subscribe(
+          // le "next" est ici et se déclenche quand la requête graphQL se passe bien
+          (result: ApolloQueryResult<UserQueryResponse>) => {
+            // le traitement sépare la requête graphQL de la donnée qui nous intéresse c'est à dire le user
+            console.log('Result : ', result)
+            const user = result.data.user
+            console.log("user :", user)
+            // ON verifie qu'il n'est pas null/vide/undefined
+            if (!!user) {
+              // on déclenche l'evt "next"
+              subscriber.next(user)
+            } else {
+              // sinon l'evt "error"
+              subscriber.error("User not found")
+            }
+          },
+          // le bloc error de graphQL
+          (error: any) => {
+            // On déclenche l'evt error, on transfère l'error graphQL
+            subscriber.error(error)
+          }
+        )
+      })
   }
 }
